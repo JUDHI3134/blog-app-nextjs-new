fa14adb2c3da1e9f05154926c5f8b39680443789
JUDHI3134 <judhistirbehera241@gmail.com>
2025-11-20 15:31:55 +0530

    start project and dark more functionality with next-themes packages

---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,28 @@
+import Container from "./Container"
+import ThemeToggle from "./ThemeToggle"
+
+const Navbar = () => {
+    return (
+        <nav className='sticky top-0 border-b z-50 bg-white dark:bg-slate-950'>
+            <Container>
+                <div className='flex justify-between items-center gap-8'>
+                <div className='flex items-center gap-1'>
+                    <div>icon</div>
+                    <div>BlogApp</div>
+                </div>
+                <div>
+                    search
+                </div>
+
+                <div className='flex items-center gap-5 sm:gap-8'>
+                    <ThemeToggle />
+                    <div>notification</div>
+                    <div>user menu</div>
+                </div>
+            </div>
+            </Container>
+        </nav>
+    )
+}
+
+export default Navbar
