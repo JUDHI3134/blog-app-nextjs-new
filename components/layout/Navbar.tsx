c67b4afd0a0a8518134075d0b139ea34c4a8b3a7
JUDHI3134 <judhistirbehera241@gmail.com>
2025-11-21 09:48:08 +0530

    create form field component

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -4,23 +4,29 @@ import ThemeToggle from "./ThemeToggle"
 import SearchInput from "./SearchInput"
 import Notifications from "./Notifications"
 import UserButton from "./UserButton"
+import Link from "next/link"
+
 
 const Navbar = () => {
     return (
         <nav className='sticky top-0 border-b z-50 bg-white dark:bg-slate-950'>
             <Container>
                 <div className='flex justify-between items-center gap-8'>
-                <div className='flex items-center gap-1'>
+                <Link href={"/"} className='flex items-center cursor-pointer'>
                     <MdNoteAlt size={24}/>
                     <div className="font-bold text-xl">JB-Blogs</div>
-                    </div>
+                    </Link>
                     
                     <SearchInput />
 
                 <div className='flex items-center gap-5 sm:gap-8'>
                     <ThemeToggle />
                     <Notifications />
-                    <UserButton />
+                        <UserButton />
+                        <>
+                            <Link href={"/login"}>Login</Link>
+                            <Link href={"/register"}>Register</Link>
+                        </>
                 </div>
             </div>
             </Container>
